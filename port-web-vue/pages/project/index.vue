e5d9d583f9449295a0c57f9532500c4f8abcf1ae
jklovetocode <106761531+jklovetocode@users.noreply.github.com>
2023-09-25 23:29:50 +0700

    jumper project

--- port-web-vue/pages/project/index.vue
+++ port-web-vue/pages/project/index.vue
@@ -2,19 +2,25 @@
   <div class="flex flex-col gap-4">
     <div class="grid grid-cols-3 gap-4">
       <div v-for="item in blog" name="book" class="opacity-0">
-        <div
-          class="relative w-[300px] h-[200px] rounded-xl border-2 border-slate-300 cursor-pointer"
-          @mouseenter="mouseEnterElement(item.t).enter"
-          @mouseleave="mouseEnterElement(item.t).leave"
-        >
+        <NuxtLink :to="item.link" target="_blank">
           <div
-            class="absolute z-[2] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-full flex justify-center items-center"
+            class="relative w-[300px] h-[200px] rounded-xl border-2 border-slate-300 cursor-pointer"
+            :class="`bg-[url('${item.pic}')] bg-cover`"
+            @mouseenter="mouseEnterElement(item.t).enter"
+            @mouseleave="mouseEnterElement(item.t).leave"
           >
-            <span v-if="mouseEnter === item.t" class="w-full text-center">
-              {{ item.t }}
-            </span>
+            <div
+              class="absolute z-[2] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-full flex justify-center items-center"
+            >
+              <div
+                v-if="mouseEnter === item.t"
+                class="w-full text-center bg-white"
+              >
+                {{ item.t }}
+              </div>
+            </div>
           </div>
-        </div>
+        </NuxtLink>
       </div>
     </div>
     <div class="flex items-center justify-center opacity-0" name="item">
@@ -29,7 +35,12 @@
 <script setup lang="ts">
 const { staggeringOpacity } = useAnime()
 const blog = ref([
-  { id: 1, t: 'Magic Book Store1', link: '' },
+  {
+    id: 1,
+    t: 'Jumper X Axelar',
+    link: 'https://jumper-x-axelar.vercel.app/',
+    pic: '/jumper.png',
+  },
   { id: 2, t: 'Magic Book Store2', link: '' },
   { id: 3, t: 'Magic Book Store3', link: '' },
   { id: 4, t: 'Magic Book Store4', link: '' },
